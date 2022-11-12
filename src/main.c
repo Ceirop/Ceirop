158ae48427497ebe8ab6f5b6205c1a190f2b8cc9
Ceirop <117931752+Ceirop@users.noreply.github.com>
2022-11-12 22:27:43 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,23 @@
 /*
-        MAIN.C FILE OF THE CEIROP PROJECT
+    MAIN.C FILE OF THE CEIROP PROJECT (CRP_INTERPRETER)
 
 NOTE: @Ceirop is an account made by @Wytek01
 */
+
+// For developing only. If you're making a mod, make sure to set this to 1 :)
+// Make sure to set it to 0 on release!
+#define DEBUG 1
+
 #include <stdio.h>
 #include <stdlib.h>
+#include "logs.h"
 
+#ifndef CEIROPVER
+ #define CEIROPVER "0.1"
+#endif
 int main()
 {
-    printf("Hello World");
-
+    Log(INFO, ("Ceirop Version: " + CEIROPVER));
+    
     return 0;
 }
