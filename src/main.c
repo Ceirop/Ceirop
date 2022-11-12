58f3799a1b6fd2966fcdf2b3b8d12ad7409ff9bb
Ceirop <117931752+Ceirop@users.noreply.github.com>
2022-11-12 19:11:51 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,14 @@
+/*
+        MAIN.C FILE OF THE CEIROP PROJECT
 
+NOTE: @Ceirop is an account made by @Wytek01
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
