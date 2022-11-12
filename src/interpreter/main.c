b0f2fb00a3d834d756d0141d5cd5e0adddf17f72
Ceirop <117931752+Ceirop@users.noreply.github.com>
2022-11-12 22:29:27 +0100

    Update and rename src/main.c to src/interpreter/main.c

--- src/interpreter/main.c
+++ src/interpreter/main.c
@@ -6,7 +6,7 @@ NOTE: @Ceirop is an account made by @Wytek01
 
 // For developing only. If you're making a mod, make sure to set this to 1 :)
 // Make sure to set it to 0 on release!
-#define DEBUG 1
+#define DEBUG 0
 
 #include <stdio.h>
 #include <stdlib.h>
