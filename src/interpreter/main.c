02a5e8ac96831b7fe88a31539c6a4b7eda5efd0a
Ceirop <117931752+Ceirop@users.noreply.github.com>
2023-01-05 16:57:02 +0100

    Update main.c

--- src/interpreter/main.c
+++ src/interpreter/main.c
@@ -4,20 +4,21 @@
 NOTE: @Ceirop is an account made by @Wytek01
 */
 
-// For developing only. If you're making a mod, make sure to set this to 1 :)
-// Make sure to set it to 0 on release!
-#define DEBUG 0
+// For developing only. If you're making a mod, make sure to define it :)
+// Make sure to not define it on release!
+
+//#define DBG
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "logs.h"
+#include "ceirop.h"
 
 #ifndef CEIROPVER
  #define CEIROPVER "0.1"
 #endif
-int main()
-{
-    Log(INFO, ("Ceirop Version: " + CEIROPVER));
-    
+int main(int argc, char** argv[]) {
+    Log(INFO, ("Ceirop Version: %s", CEIROPVER));
+    Log(INFO, ("Selected file: %s", argv[1]));
     return 0;
 }
